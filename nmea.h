322ea83b6de09e96318ee4eb8fb3ca97583f10e5
Bocchio <guido.bocchio@openmailbox.org>
2017-11-14 11:43:49 -0300

    Changed nmea

--- nmea.h
+++ nmea.h
@@ -1,1 +1,14 @@
-functions_interface_t functions_interface;
+#ifndef NMEA__H
+#define NMEA__H
+
+#include "types.h"
+
+typedef enum {
+	FORMAT_KML,
+	FORMAT_CSV
+} file_format;
+
+status_t parse_NMEA();
+status_t parse_file_format(string *, file_format *);
+
+#endif
