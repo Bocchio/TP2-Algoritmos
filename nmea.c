8d6caaa15951d3722c0c6c11b2099202d7b9daa1
Bocchio <guido.bocchio@openmailbox.org>
2017-11-14 12:09:02 -0300

    Changed nmea

--- nmea.c
+++ nmea.c
@@ -1,7 +1,14 @@
 #include "nmea"
 
+
+
 status_t parse_NMEA()
 {
-	
+
 	return OK;
+}
+
+status_t parse_file_format(const string *, file_format_t *)
+{
+
 }
--- nmea.h
+++ nmea.h
@@ -1,8 +1,6 @@
 #ifndef NMEA__H
 #define NMEA__H
 
-<<<<<<< HEAD
-=======
 #include "types.h"
 
 typedef enum {
@@ -11,7 +9,6 @@ typedef enum {
 } file_format_t;
 
 status_t parse_NMEA();
-status_t parse_file_format(string *, file_format_t *);
+status_t parse_file_format(const string, file_format_t *);
 
->>>>>>> 16e32f3b15c17a4f39fbc5dd24a2f17d7b361b06
 #endif
