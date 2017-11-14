a1273973cfdce1bffd9123d54219a04e7e26c268
Bocchio <guido.bocchio@openmailbox.org>
2017-11-14 13:23:30 -0300

    Commit

--- vector.h
+++ vector.h
@@ -17,6 +17,7 @@ typedef struct {
 status_t ADT_Vector_new(ADT_Vector_t **vector, functions_interface_t *element_functions);
 status_t ADT_Vector_delete(ADT_Vector_t **vector);
 status_t ADT_Vector_append(ADT_Vector_t *vector, void *new_item);
+status_t ADT_Vector_export_as_csv(ADT_Vector_t *vector);
 void * ADT_Vector_get_element(const ADT_Vector_t *vector, size_t pos);
 
 #endif
