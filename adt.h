ef8c7816fc9edd30356abd780fd7ed05f85c5965
Mivircruz <33661184+Mivircruz@users.noreply.github.com>
2017-11-21 14:12:43 -0300

    Update adt.h

--- adt.h
+++ adt.h
@@ -7,11 +7,4 @@ typedef status_t (*destructor_t)(void **);
 typedef void * (*clonator_t)(const void *);
 typedef status_t (*printer_t)(const void *, void *ctx, FILE *fo);
 
-typedef struct {
-	destructor_t destructor;
-	clonator_t clonator;
-	printer_t csv_exporter;
-	printer_t xml_exporter;
-} functions_interface_t;
-
 #endif
