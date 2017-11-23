bba2fd73cc585c1b6e23cf228e0f5767fb9a60ef
Bocchio <guido.bocchio@openmailbox.org>
2017-11-23 13:12:50 -0300

    goodbye xml.c .h

--- utils.h
+++ utils.h
@@ -4,9 +4,6 @@
 #include <stdio.h>
 #include "types.h"
 
-#define INIT_CHOP	20
-#define CHOP_SIZE	25
-
 status_t readline(FILE *fi, char **s, bool_t *eof);
 status_t strdup(const_string src, string *dest);
 status_t split(const_string src, string **dest, string delim);
