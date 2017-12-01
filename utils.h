57849e448e18bea92cec8924cc4a0216b367d0e1
Mivircruz <33661184+Mivircruz@users.noreply.github.com>
2017-12-01 02:04:50 -0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -9,7 +9,7 @@
 
 status_t readline(FILE *fi, char **s, bool_t *eof);
 status_t readfile(FILE *fi, char **s);
-status_t strdup(const char * src, string *dest);
+status_t strdup(const char * src, string *target);
 status_t split(const char * src, char ***dest, char *delim);
 status_t free_string_array(string **s);
 
