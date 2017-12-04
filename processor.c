040b899c1f68c8c51d0757db4b84589577c5b3a0
Mivircruz <33661184+Mivircruz@users.noreply.github.com>
2017-12-04 02:51:02 -0300

    Update processor.c

--- processor.c
+++ processor.c
@@ -3,6 +3,13 @@
 #include "nmea.h"
 #include "vector.h"
 
+printer_t printers[]={
+  NMEA_export_as_csv,
+  NMEA_export_as_kml
+};
+
+void *context;
+
 status_t process_gps_file(FILE*fi,FILE*fo,ADT_Vector_t * gga_vector,file_format_t file_format){
   status_t st;
   
@@ -12,7 +19,7 @@ status_t process_gps_file(FILE*fi,FILE*fo,ADT_Vector_t * gga_vector,file_format_
   
    if((st = ADT_GGA_load_fields(fi, &gga_vector)) != OK)
         return st;
-    if((st = ADT_GGA_export_fields(gga_vector, config.file_format, fo)) != OK)
+    if((st = printers[config.file_format](gga_vector,context,fo)) != OK)
         return st;
     return OK;
 }
