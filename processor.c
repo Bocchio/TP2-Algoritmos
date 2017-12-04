532cef72c0cc5dc988d9ca04c8c150cd624d8ca6
Mivircruz <33661184+Mivircruz@users.noreply.github.com>
2017-12-04 01:51:19 -0300

    Update processor.c

--- processor.c
+++ processor.c
@@ -10,9 +10,9 @@ status_t process_gps_file(FILE*fi,FILE*fo,ADT_Vector_t * gga_vector,file_format_
   if(gga_vector==NULL||fi==NULL||fo==NULL)
        return ERROR_NULL_POINTER;
   
-   if((st = parse_NMEA(fi, &gga_vector)) != OK)
+   if((st = ADT_GGA_load_fields(fi, &gga_vector)) != OK)
         return st;
-    if((st = export_NMEA(gga_vector, config.file_format, fo)) != OK)
+    if((st = ADT_GGA_export_fields(gga_vector, config.file_format, fo)) != OK)
         return st;
     return OK;
 }
