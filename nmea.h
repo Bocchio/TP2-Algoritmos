88983bf8885249bf9f7b46478ca28f6d95d01d70
Bocchio <guido.bocchio@openmailbox.org>
2017-12-08 21:02:13 -0300

    nombres y más desacoplamiento

--- nmea.h
+++ nmea.h
@@ -10,18 +10,22 @@ typedef struct{
 	double altitude;
 } ADT_NMEA_record_t;
 
+/* constructors */
+status_t ADT_NMEA_record_new(ADT_NMEA_record_t **nmea_record);
+status_t ADT_NMEA_record_new_from_strings(ADT_NMEA_record_t **nmea_record, string *fields);
+
+/* setters */
+status_t ADT_NMEA_record_set_latitude(ADT_NMEA_record_t *nmea_record, double latitude);
+status_t ADT_NMEA_record_set_longitude(ADT_NMEA_record_t *nmea_record, double longitude);
+status_t ADT_NMEA_record_set_altitude(ADT_NMEA_record_t *nmea_record, double altitude);
+
 status_t NMEA_export_as_csv(const ADT_Vector_t *vector, void *context, FILE *fo);
 status_t NMEA_export_as_kml(const ADT_Vector_t *vector, void * context, FILE *fo);
 
-status_t ADT_NMEA_record_parse_latitude(string coord, double *degrees, bool_t *is_empty);
-status_t ADT_NMEA_record_parse_longitude(string coord, double *degrees, bool_t *is_empty);
-status_t ADT_NMEA_record_new_from_strings(ADT_NMEA_record_t **nmea_record, string *fields);
-status_t ADT_NMEA_record_delete_fields(ADT_NMEA_record_t **nmea_record);
-status_t ADT_NMEA_record_load_fields(FILE *fi, ADT_Vector_t **gga);
+/* destructor */
+status_t ADT_NMEA_record_delete(ADT_NMEA_record_t **nmea_record);
+
 status_t ADT_NMEA_record_export_as_kml(const ADT_NMEA_record_t *nmea_record, void *ctx, FILE *fo);
 status_t ADT_NMEA_record_export_as_csv(const ADT_NMEA_record_t *nmea_record, void *ctx, FILE *fo);
-
-status_t NMEA_get_kml_ctx(void **ctx);
-status_t NMEA_destroy_kml_ctx(void **_ctx);
 
 #endif
